15207c1e9894c9bb2df64268e6dfae091418fa28
Kurt Obando <kurtobando.developer@gmail.com>
2019-04-20 04:56:48 +0000

    about, and project page

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#navigation_icon_close").addEventListener('click', function(){
+	document.querySelector(".navigation-offset").style = "display: none;"
+});
+document.querySelector("#navigation_icon").addEventListener('click', function(){
+	document.querySelector(".navigation-offset").style = "display: block;"
+});
