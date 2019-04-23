2168a8025897cfb2043e2750f9e48cd224c462d6
Kurt Obando <kurtobando.developer@gmail.com>
2019-04-23 07:28:11 +0000

    alter webpack config to development mode instead

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+	mode: "development", // "production" | "development" | "none"
 	entry: './src/js/index.js',
 	output: {
 		filename: 'bundle.js',
