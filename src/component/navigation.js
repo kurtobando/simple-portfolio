9be798b989fbab7c444c7329359bb70d322fbedf
Kurt Obando <kurtobando.developer@gmail.com>
2020-08-12 14:22:06 +0800

    add style navigation

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -4,9 +4,11 @@ import { Link } from "gatsby"
 const Navigation = () => {
 	return(
 		<Fragment>
-			<Link to="/">about</Link>
-			<Link to="/contact">contact</Link>
-			<Link to="/project">project</Link>
+			<div className="d-flex flex-wrap justify-content-end">
+				<Link className="m-2" to="/">about</Link>
+				<Link className="m-2" to="/contact">contact</Link>
+				<Link className="m-2" to="/project">project</Link>
+			</div>
 		</Fragment>
 	)
 }
