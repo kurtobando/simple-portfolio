c708bb9ea4a0cf64a656aad6ce75ffba480cc051
Kurt Obando <kurtobando.developer@gmail.com>
2020-08-14 23:46:24 +0800

    add more padding top in navigation component

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 const Navigation = () => {
 	return(
 		<Fragment>
-			<div className="d-flex flex-wrap justify-content-end font-weight-bold navigation" >
+			<div className="d-flex flex-wrap justify-content-end font-weight-bold navigation pt-3" >
 				<Link className="m-2 text-decoration-none" to="/" activeClassName="active">about</Link>
 				<Link className="m-2 text-decoration-none" to="/contact" activeClassName="active">contact</Link>
 				<Link className="m-2 text-decoration-none" to="/project" activeClassName="active">project</Link>
