aa6068bcbd7c438d29c58645b6ece2835b48860e
Kurt Obando <kurtobando.developer@gmail.com>
2020-08-14 23:41:03 +0800

    use display classes in headings and important text instead of font-size embedeed in style attr

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ const Home = () => {
             <TemplateDefault>
                 <div className="d-flex flex-column justify-content-end" style={{ minHeight : "70vh" }}>
                    <div className="text-center text-md-left">
-                       <h1 className="font-weight-bold" style={{ fontSize: "64px" }}>{data.site.siteMetadata.title}</h1>
-                       <p style={{ fontSize: "36px" }} >{data.site.siteMetadata.description}</p>
+                       <h1 className="font-weight-bold display-1" >{data.site.siteMetadata.title}</h1>
+                       <p className="display-3" >{data.site.siteMetadata.description}</p>
                        <AccentLine />
                        <div className="my-3">
                            <SocialMediaList />
