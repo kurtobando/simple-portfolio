e19b5e91f78a538052c60d997a1ac6a417c1a5ac
Kurt Obando <kurtobando.developer@gmail.com>
2020-08-14 22:23:25 +0800

    commit contact form in contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,32 @@
 import React, { Fragment } from "react"
 import TemplateDefault from "../component/template.default"
+import SocialMediaList from "../component/social.media.list"
+import AccentLine from "../component/accent.line";
 
 const Contact = () => {
 	return (
 		<Fragment>
 			<TemplateDefault>
-				<h1>Do you see potential on my works? Let me know. </h1>
-				<form action="">
-					<input type="text" placeholder="name"/>
-					<input type="email" placeholder="email address" />
-					<textarea rows="10"  placeholder="message"/>
-					<input type="submit" value="send message"/>
-				</form>
+				<div className="d-flex flex-column justify-content-center" style={{ minHeight : "90vh" }}>
+					<div className="text-center">
+						<h1>Want to work together ? </h1>
+						<AccentLine/>
+						<p>I work best with people who are passionate about what they are working on. If you are excited about something you are making. I want to work with you.</p>
+						<SocialMediaList />
+						<form className="d-flex flex-column" >
+							<div className="form-group">
+								<input className="form-control" type="text" placeholder="name" />
+							</div>
+							<div className="form-group">
+								<input className="form-control" type="email" placeholder="email address" />
+							</div>
+							<div className="form-group">
+								<textarea className="form-control" rows="10"  placeholder="message" />
+							</div>
+							<input className="btn btn-primary btn-block" type="submit" value="send message" />
+						</form>
+					</div>
+				</div>
 			</TemplateDefault>
 		</Fragment>
 	)
