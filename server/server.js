60fe721b906f330237a008b001da717450981068
Kurt Obando <kurtobando.developer@gmail.com>
2021-05-13 11:17:59 +0800

    remove .env variable for the cert key, cert and is_https, no longer needed given we are running node behind passenger

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`
+})
 
 const URL           = process.env.EXPRESS_URL || "http://localhost"
 const PORT          = process.env.EXPRESS_PORT || 3000
-const IS_HTTPS      = process.env.EXPRESS_IS_HTTPS || false
-const KEY           = process.env.EXPRESS_KEY || null
-const CERT          = process.env.EXPRESS_CERT || null
-
 const express       = require('express')
 const helmet        = require('helmet')
 const compression   = require('compression')
@@ -13,12 +11,7 @@ const cors          = require('cors')
 const fs            = require('fs')
 const app           = express()
 
-const server = IS_HTTPS == true ?
-	require('https').createServer({
-		key : fs.readFileSync( KEY ),
-		cert : fs.readFileSync( CERT )
-	}, app ) :
-	require('http').createServer( app )
+const server = require('http').createServer( app )
 
 app.use( helmet() )
 app.use( compression() )
