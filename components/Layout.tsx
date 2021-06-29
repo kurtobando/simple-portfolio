9923f91aa851259b0927e56a1bb63004add21ad0
Kurt Obando <kurtobando.developer@gmail.com>
2021-06-29 15:50:49 +0800

    initial meta tags in Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,23 @@
+import Head from "next/head"
 import Navigation from "./Navigation"
 import styles from "./Layout.module.css"
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_AUTHOR } from "../config/config"
 
 export default function Layout({ children }) {
     return (
-        <div className={styles.Layout}>
-            <div className={styles.BrandWithNavigation}>
-                <div className="py-4 font-bold">kurtobando</div>
-                <Navigation />
+        <>
+            <Head>
+                <title>{SITE_TITLE}</title>
+                <meta name="description" content={SITE_DESCRIPTION} />
+                <meta name="author" content={SITE_AUTHOR} />
+            </Head>
+            <div className={styles.Layout}>
+                <div className={styles.BrandWithNavigation}>
+                    <div className="py-4 font-bold">kurtobando</div>
+                    <Navigation />
+                </div>
+                <div>{children}</div>
             </div>
-            <div>{children}</div>
-        </div>
+        </>
     )
 }
