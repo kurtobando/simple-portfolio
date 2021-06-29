eef8abf51b5faf0fb191ca4400e43eb0696a62a9
Kurt Obando <kurtobando.developer@gmail.com>
2021-06-29 14:51:55 +0800

    initial styles for the Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import Navigation from "./Navigation"
+import styles from "./Layout.module.css"
 
 export default function Layout({ children }) {
     return (
-        <div>
-            <div>
+        <div className={styles.Layout}>
+            <div className={styles.BrandWithNavigation}>
+                <div className="py-4 font-bold">kurtobando</div>
                 <Navigation />
             </div>
             <div>{children}</div>
