24db84de274d028ed7ed405b592917609b84199e
Kurt Obando <kurtobando.developer@gmail.com>
2021-06-30 19:00:21 +0800

    initial styles for the index component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,20 @@ export default function Home(): JSX.Element {
     return (
         <div>
             <Layout>
-                <h1>Hey! I’m Kurt</h1>
-                <p>
-                    a frontend developer who enjoy the experience working with JavaScript, and travel during spare time.
-                </p>
-                <SocialMediaList />
+                <div className="text-center ">
+                    <div className="h-screen flex flex-col justify-center">
+                        <h1 className="font-bold text-3xl">Hey! I’m Kurt</h1>
+                        <div className="mx-auto w-5/6 md:w-4/6 lg:w-3/6">
+                            <p>
+                                a frontend developer who enjoy the experience working with JavaScript, and travel during
+                                spare time.
+                            </p>
+                        </div>
+                        <div className="mx-auto py-4">
+                            <SocialMediaList />
+                        </div>
+                    </div>
+                </div>
             </Layout>
         </div>
     )
