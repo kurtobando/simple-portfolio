7c326bb6ae58200c9a8ea267b68fbc1d170aa28f
Kurt Obando <kurtobando.developer@gmail.com>
2021-06-30 20:48:33 +0800

    add similar styles for the contact and index pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,19 +1,25 @@
 import Layout from "../components/Layout"
+import SocialMediaList from "../components/SocialMediaList"
 
 export default function Contact(): JSX.Element {
     return (
         <div>
             <Layout>
-                <h1>Want to work together ?</h1>
-                <p>
-                    I work best with people who enjoy what they are working on. If you are excited about something you
-                    are making. I want to work with you.
-                </p>
-                <ul>
-                    <li>github</li>
-                    <li>linkedIn</li>
-                    <li>dribble</li>
-                </ul>
+                <div
+                    className="flex flex-col justify-center text-center"
+                    style={{ height: "80vh", minHeight: "400px" }}>
+                    <h1 className="font-bold text-3xl">Want to work together ?</h1>
+                    <div className="mx-auto w-5/6 md:w-4/6 lg:w-3/6">
+                        <p>
+                            I work best with people who enjoy what they are working on. If you are excited about
+                            something you are making. I want to work with you.
+                        </p>
+                    </div>
+                    <div className="py-4">
+                        <SocialMediaList />
+                    </div>
+                </div>
+                <div></div>
             </Layout>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,18 @@ export default function Home(): JSX.Element {
     return (
         <div>
             <Layout>
-                <div className="text-center ">
-                    <div className="h-screen flex flex-col justify-center">
-                        <h1 className="font-bold text-3xl">Hey! I’m Kurt</h1>
-                        <div className="mx-auto w-5/6 md:w-4/6 lg:w-3/6">
-                            <p>
-                                a frontend developer who enjoy the experience working with JavaScript, and travel during
-                                spare time.
-                            </p>
-                        </div>
-                        <div className="py-4">
-                            <SocialMediaList />
-                        </div>
+                <div
+                    className="flex flex-col justify-center text-center"
+                    style={{ height: "80vh", minHeight: "400px" }}>
+                    <h1 className="font-bold text-3xl">Hey! I’m Kurt</h1>
+                    <div className="mx-auto w-5/6 md:w-4/6 lg:w-3/6">
+                        <p>
+                            a frontend developer who enjoy the experience working with JavaScript, and travel during
+                            spare time.
+                        </p>
+                    </div>
+                    <div className="py-4">
+                        <SocialMediaList />
                     </div>
                 </div>
             </Layout>
