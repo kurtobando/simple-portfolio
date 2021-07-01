365b0eeaa3783d94e075830ea27e547a7b8c389b
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-01 13:29:51 +0800

    increase min-height for the contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,7 +8,7 @@ export default function Contact(): JSX.Element {
             <Layout>
                 <div
                     className="flex flex-col justify-center text-center"
-                    style={{ height: "80vh", minHeight: "400px" }}>
+                    style={{ height: "80vh", minHeight: "600px" }}>
                     <h1 className="font-bold text-3xl">Want to work together ?</h1>
                     <div className="mx-auto w-5/6 md:w-4/6 lg:w-3/6">
                         <p>
