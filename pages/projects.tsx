e02ab2cb1b01764536a8c202963f0e6836b5015d
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-03 18:27:00 +0800

    add getstaticprops add component to display projects initially

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,65 @@
+import axios from "axios"
+import Link from "next/link"
+import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
+import { GRAPHQL_URL } from "../config/config"
 
-export default function Projects(): JSX.Element {
+type ProjectNode = {
+    id: string
+    title: string
+    uri: string
+    excerpt: string
+}
+
+interface Project {
+    node: ProjectNode
+}
+
+function fetchGraphqlProjects(): string {
+    return `query fetchGraphqlProjects {
+        projects {
+          edges {
+            node {
+              id
+              uri
+              title
+              excerpt
+              date
+            }
+          }
+        }
+      }
+      `
+}
+
+export default function Projects({ projects }): JSX.Element {
     return (
         <div>
-            <Layout>Projects page</Layout>
+            <Layout>
+                {projects.map((project: Project): JSX.Element => {
+                    return (
+                        <div key={project.node.id}>
+                            <Link href={project.node.uri}>
+                                <a>
+                                    <h2>{project.node.title}</h2>
+                                </a>
+                            </Link>
+                            <div dangerouslySetInnerHTML={{ __html: project.node.excerpt }} />
+                        </div>
+                    )
+                })}
+            </Layout>
         </div>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await axios.post(GRAPHQL_URL, { query: fetchGraphqlProjects() })
+    const projects: object[] = data?.data?.projects?.edges
+
+    return {
+        props: {
+            projects,
+        },
+    }
 }
