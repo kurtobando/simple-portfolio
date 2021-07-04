6683c99504d1724ee0b83fcd53d85f23de4efae1
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-04 13:05:19 +0800

    style navigation if href is the current path

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link"
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router"
 import styles from "./Navigation.module.css"
 
 function isCurrentPath(givenPath: string): boolean {
-    const { asPath = '/' } = useRouter()
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { asPath = "/" } = useRouter()
 
-    let toComparePath: string[] | string = givenPath.split('/')
-        toComparePath = `/${toComparePath[1]}`
-    
-    let currentPath: string[] | string = asPath.split('/')
-        currentPath = `/${currentPath[1]}`  
+    let toComparePath: string[] | string = givenPath.split("/")
+    toComparePath = `/${toComparePath[1]}`
+
+    let currentPath: string[] | string = asPath.split("/")
+    currentPath = `/${currentPath[1]}`
 
     return currentPath === toComparePath
-} 
+}
 
 export default function Navigation(): JSX.Element {
     const URL = {
@@ -20,22 +21,22 @@ export default function Navigation(): JSX.Element {
         PROJECTS: `/projects`,
         CONTACT: `/contact`,
     }
-    
+
     return (
         <ul className={styles.Navigation}>
             <li>
                 <Link href={URL.HOME}>
-                    <a>about</a>
+                    <a className={isCurrentPath(URL.HOME) ? styles.isCurrentPath : ""}>about</a>
                 </Link>
             </li>
             <li>
                 <Link href={URL.PROJECTS}>
-                    <a>projects</a>
+                    <a className={isCurrentPath(URL.PROJECTS) ? styles.isCurrentPath : ""}>projects</a>
                 </Link>
             </li>
             <li>
                 <Link href={URL.CONTACT}>
-                    <a>contact</a>
+                    <a className={isCurrentPath(URL.CONTACT) ? styles.isCurrentPath : ""}>contact</a>
                 </Link>
             </li>
         </ul>
