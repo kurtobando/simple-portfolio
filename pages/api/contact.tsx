572dc8ca555792e45c1d19677e1df67aeb9a3b93
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-05 13:55:32 +0800

    change contact.logs directory

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -14,7 +14,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const { name = "", email_address = "", message = "" }: formData = req.body
 
     fs.appendFileSync(
-        "./public/contact.logs",
+        "./contact.logs",
         `${new Date()}{ "name": "${name}", "email": "${email_address}", "message": "${message}" } \n`
     )
 
@@ -40,11 +40,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     mailer({ name, message, email: email_address })
         .then((data) => {
-            fs.appendFileSync("./public/contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(data)} } \n`)
+            fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(data)} } \n`)
             return res.status(200).json({ message: "Message sent!" })
         })
         .catch((error) => {
-            fs.appendFileSync("./public/contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(error)} } \n`)
+            fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(error)} } \n`)
             return res.status(200).json({ message: "Message sent!" })
         })
 }
