44998482d3fc66a89bc9d56774193bdfe5741d03
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-05 14:45:51 +0800

    comment out logging function

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -13,10 +13,10 @@ interface formData {
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const { name = "", email_address = "", message = "" }: formData = req.body
 
-    fs.appendFileSync(
-        "./contact.logs",
-        `${new Date()}{ "name": "${name}", "email": "${email_address}", "message": "${message}" } \n`
-    )
+    // fs.appendFileSync(
+    //     "./contact.logs",
+    //     `${new Date()}{ "name": "${name}", "email": "${email_address}", "message": "${message}" } \n`
+    // )
 
     if (req.method === "GET") {
         return res.status(403).json({ message: `GET request declined` })
@@ -40,11 +40,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     mailer({ name, message, email: email_address })
         .then((data) => {
-            fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(data)} } \n`)
+            // fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(data)} } \n`)
             return res.status(200).json({ message: "Message sent!" })
         })
         .catch((error) => {
-            fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(error)} } \n`)
+            // fs.appendFileSync("./contact.logs", `${new Date()}{ "nodemailer": ${JSON.stringify(error)} } \n`)
             return res.status(200).json({ message: "Message sent!" })
         })
 }
