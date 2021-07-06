3f3a5f6a3f001b24eb3b21d868e3a2c2c38a2512
Kurt Obando <kurtobando.developer@gmail.com>
2021-07-06 14:33:39 +0800

    quick fix for the 'gtag' does not exist on type 'Window & typeof globalThis'

--- lib/analytics.tsx
+++ lib/analytics.tsx
@@ -1,5 +1,13 @@
 import { GOOGLE_ANALYTICS_TRACKING_ID } from "../config/config"
 
+// issue    : Property 'gtag' does not exist on type 'Window & typeof globalThis'
+// solution : https://stackoverflow.com/questions/12709074/how-do-you-explicitly-set-a-new-property-on-window-in-typescript
+declare global {
+    interface Window {
+        gtag: any
+    }
+}
+
 // log the pageview with their URL
 export const pageview = (url: string): void => {
     window.gtag("config", GOOGLE_ANALYTICS_TRACKING_ID, {
